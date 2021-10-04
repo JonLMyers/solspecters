32d5d7e20230f50e5f218325bd788123f97e00be
JonLMyers <jlm4508@rit.edu>
2021-10-03 22:17:30 -0700

    updates

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import React from "react";
-import HttpsRedirect from 'react-https-redirect'
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
-import sslRedirect from 'heroku-ssl-redirect';
-import express from 'express';
-const app = express();
-app.use(sslRedirect());
 
 ReactDOM.render(
   <React.StrictMode>
